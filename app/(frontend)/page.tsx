986810ef8202b0c2c708dbb08c02d609b80fb99d
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-10-02 18:18:16 +0300

    upd

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getPayload } from "payload";
 import config from "@/payload.config";
+import Link from "next/link";
 
 export default async function Home() {
   const payload = await getPayload({ config });
@@ -86,9 +87,9 @@ export default async function Home() {
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Перейдите в{" "}
-            <a href="/admin" className="text-blue-600 dark:text-blue-400 hover:underline">
+            <Link href="/admin" className="text-blue-600 dark:text-blue-400 hover:underline">
               панель администратора (/admin)
-            </a>
+            </Link>
           </li>
           <li className="tracking-[-.01em]">
             Откройте раздел &quot;Настройки сайта&quot; и заполните поля
@@ -96,21 +97,21 @@ export default async function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/admin"
             rel="noopener noreferrer"
           >
             Админ панель
-          </a>
-          <a
+          </Link>
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://payloadcms.com/docs"
             target="_blank"
             rel="noopener noreferrer"
           >
             Payload Docs
-          </a>
+          </Link>
         </div>
       </main>
     </div>
